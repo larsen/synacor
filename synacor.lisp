891df21d9aa649a332cc5882be93f0842f4fc909
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2021-03-07 18:02:23 +0100

    test function
    I also had to tweak the memory content for the self-test to proceed.
    Not sure this is a bug in my code or part of the challenge

--- synacor.lisp
+++ synacor.lisp
@@ -2,8 +2,10 @@
 
 (in-package #:synacor)
 
-;; (when (not *cpu*)
-;;   (setf *cpu* (make-instance 'cpu)))
-;; (reset-cpu! *cpu*)
-;; (load! (read-bin-file *challenge-bin*) *cpu*)
-;; (run! *cpu*)
+(defun test-machine (&key (with-instruction-log nil))
+  (setf *cpu* (make-instance 'cpu))
+  (reset-cpu! *cpu*)
+  (load! (read-bin-file *challenge-bin*) *cpu*)
+  ;; Why I had to add this?
+  (set-address! 865 844 *cpu*)
+  (run! *cpu* :with-instruction-log with-instruction-log))
